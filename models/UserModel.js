7396c8c3f3aa0be27f368d05491c757f8fd35ae7
WendyJohaRA <wendy.roncancio.ja@gmail.com>
2022-11-27 21:18:41 -0500

    Entrega

--- models/UserModel.js
+++ models/UserModel.js
@@ -14,12 +14,3 @@ let UserSchema = new Schema({
 })
 
 module.exports = mongoose.model('Cliente', UserSchema, 'Clientes')
-
-const ProductSchema = new Schema({
-    name: { type: String, required: true, unique: true },
-    img: { type: String, required: true },
-    inCart: { type: Boolean, default: false },
-    price: { type: Number, required: true },
-  });
-  
-  module.exports = mongoose.model("Product", ProductSchema, 'Products');
